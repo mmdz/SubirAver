dbe050b41dd93739ee1833aac1a79eb1ac507664
mmdz <mellmendez4089@gmail.com>
2017-09-11 10:02:28 -0300

    subiendo

--- workspace/Disco2/test/ar/edu/unlam/ar/TestDisco2.java
+++ workspace/Disco2/test/ar/edu/unlam/ar/TestDisco2.java
@@ -9,38 +9,38 @@ import ar.edu.unlam.pb2.CuentaGanado;
 import ar.unlam.edu.ar.Disco2;
 
 public class TestDisco2 {
-	
-	@Test
-	public void testQueCalculaElPerimetroInterior() {
-		
-		Disco2 miDisco2 = new Disco2(2.0, 4.0);
-		Double perimetroActual = miDisco2.calcularPerimetroInterior(); 
-		Double perimetroEsperado = 12.56;
-		
-		assertEquals(perimetroEsperado,perimetroActual,0.01);
-	}
-	
-	@Test 
-	public void testQueCalculaElPerimetroExterior(){
-		
-		Disco2 miDisco2 = new Disco2(2.0,4.0);
-		Double perimetroActual = miDisco2.calcularPerimetroExterior();
-		Double perimetroEsperado = 25.13;
-		
-		assertEquals(perimetroEsperado,perimetroActual,0.01);
-	}
-	
-	@Test 
-	public void testQueCalculaLaSuperficie(){
-		
-		Disco2 miDisco2 = new Disco2(2.0, 4.0);
-		Double superficieActual = miDisco2.calcularSuperficie();
-		Double superficieEsperada = 50.26;
-		
-		assertEquals(superficieEsperada,superficieActual,0.01);
-	}
-	
-}
+//	
+//	@Test
+//	public void testQueCalculaElPerimetroInterior() {
+//		
+//		Disco2 miDisco2 = new Disco2(2.0, 4.0);
+//		Double perimetroActual = miDisco2.calcularPerimetroInterior(); 
+//		Double perimetroEsperado = 12.56;
+//		
+//		assertEquals(perimetroEsperado,perimetroActual,0.01);
+//	}
+//	
+//	@Test 
+//	public void testQueCalculaElPerimetroExterior(){
+//		
+//		Disco2 miDisco2 = new Disco2(2.0,4.0);
+//		Double perimetroActual = miDisco2.calcularPerimetroExterior();
+//		Double perimetroEsperado = 25.13;
+//		
+//		assertEquals(perimetroEsperado,perimetroActual,0.01);
+//	}
+//	
+//	@Test 
+//	public void testQueCalculaLaSuperficie(){
+//		
+//		Disco2 miDisco2 = new Disco2(2.0, 4.0);
+//		Double superficieActual = miDisco2.calcularSuperficie();
+//		Double superficieEsperada = 50.26;
+//		
+//		assertEquals(superficieEsperada,superficieActual,0.01);
+//	}
+//	
+//}
 
 
 
